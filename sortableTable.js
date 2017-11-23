7f057afea7087a6388f8a04496db8fb29111c389
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2017-11-23 15:46:52 +0100

    Object Oriented Refactoring

--- sortableTable.js
+++ sortableTable.js
@@ -1,82 +1,119 @@
 
-var columnfilter=[];
+function sortableTable (tbl,tableid,filterid,caption,renderCell,renderSortOptions,renderColumnFilter,rowFilter) {
 
-function renderTable(tbl,tableid,filterid,caption)
-{
-
-		// Global that contains rendered html for column filter div
-		columnfilter = JSON.parse(localStorage.getItem("filtercolnames"));
-
-		if(columnfilter == null) columnfilter=[];
-
-		var filterstr="";
-		for(let colno in tbl.tblhead){
-				var col=tbl.tblhead[colno];
-				filterstr+=renderColumnFilter(col,columnfilter.indexOf(col)>-1);
+		this.columnfilter=[];
+		this.sortcolumn="UNK";
+		this.sortstatus=true;
+		this.tbl=tbl;
+		this.tableid=tableid;
+		this.filterid=filterid;
+		this.caption=caption;
+		this.renderCell=renderCell;
+		this.renderSortOptions=renderSortOptions;
+		this.renderColumnFilter=renderColumnFilter;
+		this.rowFilter=rowFilter;
+				
+		alert("Hello!");
+				
+		function renderTable()
+		{
+				reRender();
 		}
-		document.getElementById(filterid).innerHTML=filterstr;
 		
-		// Global that contains rendered html for table
-		
-		var str="";
-		
-		str+= "<caption></caption>";
-		
-		str+= "<thead>";
-			str+= "<tr>";
-			for(let colno in tbl.tblhead){
-					var col=tbl.tblhead[colno];
-					if(columnfilter.indexOf(col)>-1){
-							str+= "<th><span style='padding:0 10px 0 10px;'>"+col+"</span></th>";
-					}
-			}
-			str+= "</tr>";
-		str+= "</thead>";
-		
-		// Render table body
-		str+= "<tbody>";
-			for(let rowno in tbl.tblbody){
-					var row=tbl.tblbody[rowno]
-					if(rowFilter(row)){
-						str+="<tr>";
-						for(let colno in row){
-							col=row[colno];
-							if(columnfilter.indexOf(tbl.tblhead[colno])>-1){
-									str+="<td>";
-									str+=renderCell(col,tbl.tblhead[colno]);
-									str+="</td>";						
-							}
-						}
-						str+="</tr>";
-					}
-			}
-		str+= "</tbody>";
+		function reRender()
+		{
+				alert("Hello!");
 
-		str+= "<tfoot style='border-top:2px solid #000'>";
-		str+= "<tr style='font-style:italic;'>";
-		for(let col in tbl.tblfoot){
-				if (tbl.tblfoot[col]!="UNK"){
-						str+="<td>"+tbl.tblfoot[col]+"</td>";
-				} else {
-						str+="<td> </td>";
+				// Global that contains rendered html for column filter div
+				this.columnfilter = JSON.parse(localStorage.getItem(this.tableid+"_filtercolnames"));
+
+				if(this.columnfilter == null) this.columnfilter=[];
+
+				var filterstr="";
+				for(let colno in this.tbl.tblhead){
+						var col=this.tbl.tblhead[colno];
+						filterstr+=this.renderColumnFilter(col,this.columnfilter.indexOf(col)>-1);
 				}
-		}
-		str+="</tr>";
-		str+= "</tfoot></table>";
+				document.getElementById(this.filterid).innerHTML=filterstr;
 
-		document.getElementById(tableid).innerHTML=str;
+				// Global that contains rendered html for table
+				var str="";
+
+				str+= "<caption>"+this.caption+"</caption>";
+
+				str+= "<thead>";
+					str+= "<tr>";
+					for(let colno in this.tbl.tblhead){
+							var col=this.tbl.tblhead[colno];
+							if(this.columnfilter.indexOf(col)>-1){
+									if(col==this.sortcolumn){
+											str+= "<th><span style='padding:0 10px 0 10px;'>"+this.renderSortOptions(col,this.sortstatus)+"</span></th>";
+									}else{
+											str+= "<th><span style='padding:0 10px 0 10px;'>"+this.renderSortOptions(col,-1)+"</span></th>";
+									}
+							}
+					}
+					str+= "</tr>";
+				str+= "</thead>";
+
+				// Render table body
+				str+= "<tbody>";
+					for(let rowno in this.tbl.tblbody){
+							var row=this.tbl.tblbody[rowno]
+							if(rowFilter(row)){
+								str+="<tr>";
+								for(let colno in row){
+									col=row[colno];
+									if(this.columnfilter.indexOf(this.tbl.tblhead[colno])>-1){
+											str+="<td>";
+											str+=this.renderCell(col,this.tbl.tblhead[colno]);
+											str+="</td>";						
+									}
+								}
+								str+="</tr>";
+							}
+					}
+				str+= "</tbody>";
+
+				str+= "<tfoot style='border-top:2px solid #000'>";
+				str+= "<tr style='font-style:italic;'>";
+				for(let col in this.tbl.tblfoot){
+						if (this.tbl.tblfoot[col]!="UNK"){
+								str+="<td>"+this.tbl.tblfoot[col]+"</td>";
+						} else {
+								str+="<td> </td>";
+						}
+				}
+				str+="</tr>";
+				str+= "</tfoot></table>";
+
+				document.getElementById(this.tableid).innerHTML=str;
+
+		}
+
+		function toggleColumn(col)
+		{
+				if(this.columnfilter.indexOf(col)==-1){
+						this.columnfilter.push(col);
+				}else{
+						this.columnfilter.splice(this.columnfilter.indexOf(col),1);
+				}
+
+				localStorage.setItem(this.tableid+"_filtercolnames", JSON.stringify(this.columnfilter));
+
+				reRender();
+		}
+
+		function toggleSortStatus(col,status)
+		{
+				this.sortcolumn=col;
+				this.sortstatus=status;
+				
+				reRender();
+		}
 
 }
 
-function toggleColumn(col,tbl,tableid,filterid)
-{
-		if(columnfilter.indexOf(col)==-1){
-				columnfilter.push(col);
-		}else{
-				columnfilter.splice(columnfilter.indexOf(col),1);
-		}
 
-		localStorage.setItem("filtercolnames", JSON.stringify(columnfilter));
 
-		renderTable(tbl,tableid,filterid);
-}
+
